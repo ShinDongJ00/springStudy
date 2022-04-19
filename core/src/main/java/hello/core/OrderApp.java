687fa0551f46615de318a1e81bd8501485999017
ShinDongJ00 <ehdwn9508@naver.com>
2022-04-19 17:39:19 +0900

    스프링으로 전환 - AppConfig에서 빈으로 등록 - 스프링컨테이너에서 빈객체 가져와서 활용 테스트

---
+++ core/src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class OrderApp {
+    public static void main (String[] args){
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        OrderService orderService = applicationContext.getBean("orderService",OrderService.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+        long memberId = 1L;
+        Member member = new Member(memberId, "member1", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(member.getId(),"item1",12000);
+
+        System.out.println("order : " + order);
+    }
+}
