e3f6d15454a84e33e3bc88bb0e94141ce13fbf81
ShinDongJ00 <ehdwn9508@naver.com>
2022-04-20 16:31:08 +0900

    컴포넌트 스캔 - 빈 대상 클래스에 @Component,@Autowired 추가

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -4,11 +4,15 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository  memberRepository;
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
