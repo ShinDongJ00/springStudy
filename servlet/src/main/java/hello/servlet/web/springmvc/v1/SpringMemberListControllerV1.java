eb8f1f1471d41a79f61e8473b1e67bf04d6e86cd
ShinDongJ00 <ehdwn9508@naver.com>
2022-04-27 16:38:08 +0900

    스프링 MVC시작 - 권장방식은 아님

---
+++ servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,23 @@
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class SpringMemberListControllerV1 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process(){
+        List<Member> memberList = memberRepository.findAll();
+
+        ModelAndView mav = new ModelAndView("members");
+        mav.addObject("memberList",memberList);
+        return mav;
+    }
+}
